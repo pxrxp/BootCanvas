0b4c40fa434e90e28506bd9b01f395c7823d593d
pxrxp <187556078+pxrxp@users.noreply.github.com>
2025-11-26 13:23:06 +0545

    Print hello world using BIOS interrupt

--- src/boot.asm
+++ src/boot.asm
@@ -2,7 +2,26 @@
 
 ; Infinite loop
 start:
+        mov si, msg_hello
+        call print_string
+        
         jmp $
+
+print_string:
+        mov ah, 0x0e
+.repeat:
+        lodsb                 ; 1 byte [si]->al
+        cmp al, 0             ; al == \0 ?
+        je .done              ; Yes => End Loop
+        int 0x10              ; No  => BIOS interrupt
+        jmp .repeat           ;        Loop back
+.done:
+        ret
+
+; 13 = \r
+; 10 = \n
+;  0 = \0
+msg_hello db 'Hello World', 13, 10, 0
 
 ; Bootloader = 512 bytes
 ; Signature i.e. AA55 = 2 bytes
