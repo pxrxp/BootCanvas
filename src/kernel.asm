038225a451007416368cfed23165a46605dd3c86
pxrxp <187556078+pxrxp@users.noreply.github.com>
2025-11-27 00:08:33 +0545

    Add linker.ld to link the .asm files (except boot.asm)

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,6 +1,8 @@
-[org 0x1000]
+section .text
+global kstart
 
-start:
+[bits 16]
+kstart:
     ; Set the video memory pointer (ES:DI)
     mov ax, 0xb800          ; Video memory start segment
     mov es, ax              ; ES = 0xb800
@@ -8,8 +10,6 @@ start:
     
     mov al, 'K'             ; Character 'K'
     mov ah, 0x24            ; Bright Green (0x2) on Black (0x0)
+    stosw                   ; Store AX (word) into ES:[DI]
     
     jmp $
-    
-; Pad remaining portion of sector 2 with 0
-times 512-($-$$) db 0
